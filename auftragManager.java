5949f4cd99afce3a211a84323e82296315aaafe3
VroniZinsser <Veronika.Zinsser@stud.uni-regensburg.de>
2014-07-03 16:42:34 +0200

    Update auftragManager.java

--- auftragManager.java
+++ auftragManager.java
@@ -1,4 +1,21 @@
 public class auftragManager {
+  
+  	public auftragManager() {
+		
+	}
+	
+	public static void addTask (Task newTask) {
+		//add new task to database
+	}
+	
+	public static void addDriver (Driver newDriver) {
+		//add new driver to database
+	}
+	
+	public static Driver getDriver (int id) {
+		//get driver with id from datavase
+	}
+	
 
 
 }
